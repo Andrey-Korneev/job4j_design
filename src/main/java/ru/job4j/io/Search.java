f5d594f9cea0a8cd6ed7333dd47ae47badb8fcca
andrey korneev <andrey.p.korneev@yandex.ru>
2024-01-13 12:39:45 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -6,11 +6,25 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        checkArgs(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    private static void checkArgs(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Wrong number of program arguments. There should be two.");
+        }
+        if (!Pattern.matches("([a-zA-Z]:\\\\)?(([a-zA-Z0-9_.-]*)\\\\?)*", args[0])) {
+            throw new IllegalArgumentException("Wrong path.");
+        }
+        if (!Pattern.matches("\\.\\w+", args[1])) {
+            throw new IllegalArgumentException("Wrong file extension.");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
